fc9df15b00fde2776f1051e3c2880169e5cf3d1a
katsu105 <zgok105@yahoo.co.jp>
2020-11-26 21:26:02 +0900

    コンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type = "text" onChange={() => {console.log("クリックした！")}} />
-      </React.Fragment>
-    )
-  }
+import React from 'react';
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type = "text" onChange={() => {console.log("クリックした！")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
 
+const Cat = () => {
+  return <div>Neow!</div>
+}
 export default App;

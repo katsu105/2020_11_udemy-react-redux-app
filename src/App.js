1878a7cf7aa8d69d99c25fdbbcaff1d9fbf49ab2
katsu105 <zgok105@yahoo.co.jp>
2020-11-26 21:21:25 +0900

    クラスコンポーネントに変更

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-  return <div>Hello, world!</div>;
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type = "text" onChange={() => {console.log("クリックした！")}} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;

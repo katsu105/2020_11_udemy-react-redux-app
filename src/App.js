f4c685e062e4326f5f42bda618bdf79c0cd57076
katsu105 <zgok105@yahoo.co.jp>
2020-11-26 21:54:58 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React from 'react';
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type = "text" onChange={() => {console.log("クリックした！")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
 
 const App = () => {
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 5}
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      { 
+        profiles.map((profile, index) =>{
+          return <User key={index} name={profile.name} age = {profile.age} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Neow!</div>
+const User = (props) => {
+  return <div>Hi!, I am {props.name}, and {props.age} years old!</div>
 }
 export default App;
